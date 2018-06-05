75046342ed4c85196e17c125ede5751c1137c011
Frank <550862872@qq.com>
2018-06-05 11:27:22 +0800

    shou feng qin

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+
+
+$(function () {//页面加载完成之后
+    $(".txts").mouseover(function () {
+        $(this).parent().stop(true, true).animate({ "width": "789px" }, 500).siblings().stop(true, true).animate({ "width": "100px" }, 500);
+    })
+})
